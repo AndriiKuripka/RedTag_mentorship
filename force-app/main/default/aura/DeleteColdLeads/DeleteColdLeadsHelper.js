a80ef085f8fc3ef7c07bfe90a79798f2df2b1afe
AndriiKuripka <51426065+AndriiKuripka@users.noreply.github.com>
2022-08-15 23:02:27 +0200

    Task_6 (Fix tabulation)

--- force-app/main/default/aura/DeleteColdLeads/DeleteColdLeadsHelper.js
+++ force-app/main/default/aura/DeleteColdLeads/DeleteColdLeadsHelper.js
@@ -1,29 +1,29 @@
 ({
-    cancel : function(cmp,event){
-        var dismissActionPanel = $A.get("e.force:closeQuickAction");
+	cancel : function(cmp,event){
+		var dismissActionPanel = $A.get("e.force:closeQuickAction");
 		dismissActionPanel.fire();
-    },
-    
+	},
+
 	successfulDelete : function(cmp, event) {
 		var secondHeaderLabel = $A.get("$Label.c.ToastTitleDelete");
 		var secondMsgLabel = $A.get("$Label.c.DeletionProcess");
-        var success = $A.get("$Label.c.Success");
+		var success = $A.get("$Label.c.Success");
 
 		var action = cmp.get("c.BatchDeleteAllLeadsCall");
 		action.setCallback(this, function(response) {
-            var state = response.getState();
-            if (state === success) {
+			var state = response.getState();
+			if (state === success) {
 				console.log('Result from server-->>> ' + response.getReturnValue());
 			}
 		});
 		$A.enqueueAction(action);
 		var toastEvent = $A.get("e.force:showToast");
 		toastEvent.setParams({
- 			"title": secondHeaderLabel,
- 			"message": secondMsgLabel,
+			"title": secondHeaderLabel,
+			"message": secondMsgLabel,
 			"type": success
- 		});
- 		toastEvent.fire();
+		});
+		toastEvent.fire();
 		var dismissActionPanel = $A.get("e.force:closeQuickAction");
 		dismissActionPanel.fire();
 	}
